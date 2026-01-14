94daa0bed303bf571868951df3b72a9a2cc86d88
Lucas Vacari <lucasrvacari99@gmail.com>
2026-01-14 17:28:11 -0300

    adiciona headers de cache-control e pragma no-cache ao redirecionar para nova conversa em /chat?c={id}, inclui header customizado X-Tuq-New-Redirect para identificar redirecionamento de nova conversa

--- app/Controllers/ChatController.php
+++ app/Controllers/ChatController.php
@@ -122,6 +122,9 @@ class ChatController extends Controller
             }
 
             $_SESSION['current_conversation_id'] = $conversation->id;
+            header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+            header('Pragma: no-cache');
+            header('X-Tuq-New-Redirect: 1');
             header('Location: /chat?c=' . $conversation->id);
             exit;
         } elseif ($conversationParam > 0) {
